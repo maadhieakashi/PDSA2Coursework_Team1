d6e2cce67d4a615ec558dc77b3f949c4ee4bd4fe
hashiniRanasinghe <chathurangihashini5@gmail.com>
2024-09-04 16:04:14 +0530

    main menu and game 1

--- GameMenu.cs
+++ GameMenu.cs
@@ -19,10 +19,36 @@ namespace PDSA2Coursework_Team1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            
               TowerofHanoi toverofHonaiForm = new TowerofHanoi();
               toverofHonaiForm.Show();
+              this.Hide();
+            
+        }
+         private void button2_Click(object sender, EventArgs e)
+        {
+              SixteenQueen_Puzzle SixteenQueen_PuzzleForm = new SixteenQueen_Puzzle();
+              SixteenQueen_PuzzleForm.Show();
+              this.Hide();
+            
+        }
+         private void button3_Click(object sender, EventArgs e)
+        {
+              MinimumCost MinimumCostForm = new MinimumCost();
+              MinimumCostForm.Show();
+              this.Hide();
+            
+        }
+         private void button4_Click(object sender, EventArgs e)
+        {
+              ShortestPath ShortestPathForm = new ShortestPath();
+              ShortestPathForm.Show();
+              this.Hide();
+            
+        }
+               private void button5_Click(object sender, EventArgs e)
+        {
+              PredictValueIndex PredictValueIndexForm = new PredictValueIndex();
+              PredictValueIndexForm.Show();
               this.Hide();
             
         }
