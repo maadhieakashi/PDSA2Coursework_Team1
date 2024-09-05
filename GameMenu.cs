b2c8dd99e75ea925168f76e4011f6b606575e4a3
maadhie Akashi <sahimeshi1266@gmail.com>
2024-09-05 20:00:24 +0530

    changes

--- GameMenu.cs
+++ GameMenu.cs
@@ -19,32 +19,32 @@ namespace PDSA2Coursework_Team1
 
         private void button1_Click(object sender, EventArgs e)
         {
-              TowerofHanoi toverofHonaiForm = new TowerofHanoi();
-              toverofHonaiForm.Show();
-              this.Hide();
-            
+            TowerofHanoi toverofHonaiForm = new TowerofHanoi();
+            toverofHonaiForm.Show();
+            this.Hide();
+
         }
 
-         private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-              SixteenQueen_Puzzle SixteenQueen_PuzzleForm = new SixteenQueen_Puzzle();
-              SixteenQueen_PuzzleForm.Show();
-              this.Hide();
-            
+            SixteenQueen_Puzzle SixteenQueen_PuzzleForm = new SixteenQueen_Puzzle();
+            SixteenQueen_PuzzleForm.Show();
+            this.Hide();
+
         }
-         private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-              MinimumCost MinimumCostForm = new MinimumCost();
-              MinimumCostForm.Show();
-              this.Hide();
-            
+            MinimumCost MinimumCostForm = new MinimumCost();
+            MinimumCostForm.Show();
+            this.Hide();
+
         }
-         private void button4_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-              ShortestPath ShortestPathForm = new ShortestPath();
-              ShortestPathForm.Show();
-              this.Hide();
-            
+            ShortestPath ShortestPathForm = new ShortestPath();
+            ShortestPathForm.Show();
+            this.Hide();
+
         }
         private void button5_Click(object sender, EventArgs e)
         {
@@ -52,7 +52,7 @@ namespace PDSA2Coursework_Team1
             PredictValueIndexForm.Show();
             this.Hide();
         }
-      
+
         private void GameMenu_Load(object sender, EventArgs e)
         {
 
